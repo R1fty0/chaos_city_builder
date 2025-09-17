9242d55edb82d3a96301290998953f8156745f81
MidnightBlue <113545024+R1fty0@users.noreply.github.com>
2025-09-17 13:47:13 -0700

    added interaction component, and outline shader

---
+++ common/interaction_component.gd
@@ -0,0 +1,47 @@
+extends Area3D
+class_name InteractionComponent
+
+# NOTE: All meshes that use this component require a weighted normals modifier in Blender. 
+# Credit: https://www.youtube.com/watch?v=CG0TMH8D8kY
+
+signal selected
+signal unselected
+
+@export_category("Outlines")
+@export var mesh: MeshInstance3D
+@export var outline_material: Material
+@export var selection_material: Material
+@export var collision_body: CollisionObject3D
+
+var is_selected: bool = false
+
+func _ready() -> void:
+	# Connect relevant signals. 
+	if collision_body:
+		collision_body.mouse_entered.connect(_on_mouse_entered)
+		collision_body.mouse_exited.connect(_on_mouse_exited)
+		collision_body.input_event.connect(_on_input_event)
+	else:
+		push_error("Missing collision body reference for outline component: " + str(name))
+
+func _on_mouse_entered() -> void:
+	# Enable the hover outline. 
+	if is_selected == false:
+		mesh.material_overlay = outline_material
+
+func _on_mouse_exited() -> void:
+	# Disable the hover outline. 
+	if is_selected == false:
+		mesh.material_overlay = null
+
+func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	# Select/Deselect Object Outline 
+	if event.is_action_pressed("select"):
+		if not is_selected:
+			mesh.material_overlay = selection_material
+			is_selected = true
+			selected.emit()
+		else:
+			mesh.material_overlay = outline_material
+			is_selected = false
+			unselected.emit()
